16acec3657bd61c536fbe2e29917ea3e1e631e57
zhumagul.mur@gmail.com <zhumagul.mur@gmail.com>
2024-03-01 15:28:16 +0600

    Initial commit

---
+++ myapp/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('email',)  
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords do not match")
+
+    
+
+        return password2
